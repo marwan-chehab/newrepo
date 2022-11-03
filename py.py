04443442a11db53b9a79ad5b466a8a40facc23e1
marwan <chehab.est@gmail.com>
2022-11-03 12:48:45 +0200

    added even odd and date

--- py.py
+++ py.py
@@ -2,5 +2,8 @@ import os
 
 for f in os.listdir():
     if f[0] == 'h':
-        os.system(f'mv {f} {f[:5] + f[5:].zfill(2)}; ls -l')
-
+        #os.system(f'mv {f} {f[:5] + f[5:].zfill(2)}; ls -l')
+        if int(f[5:]) % 2 == 0:
+            os.system(f'echo {f} is even. >> {f}')
+        else:
+            os.system(f'echo {f} is odd. >> {f}')
